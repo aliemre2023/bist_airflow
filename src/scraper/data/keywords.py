cb1abbd52a60d54e693006f91af2f5371987b1e5
aliemre2023 <aliemrekaya239@gmail.com>
2026-03-01 21:23:53 +0300

    news added

--- src/scraper/data/keywords.py
+++
@@ -1,7 +0,0 @@
-positive = ["olumlu", "yüksel", "kazan", "art", "yüksel", "artmakta", 
-            "gelişmekte", "ilerlemekte", "yükselmekte", "yükselen", 
-            "artışta", "artıyor", "iyileşmekte", "pozitif", "olumlu", 
-            "iyi", "harika", "mükemmel"]
-negative = ["az", "olumsuz", "düş", "kayıp", "kayb", "düşüşte", "azalmakta", 
-            "gerilemekte", "kötüleşmekte", "düşen", "azalışta", "azalıyor", 
-            "kötüleşmekte", "negatif", "olumsuz", "kötü", "berbat"]
